ee8cc214dd5add14f0c30ce98e48a6baf69d0c86
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-01-13 18:16:55 +0100

    Codi final del vídeo 129

--- app/Listeners/SendVideoCreatedNotification.php
+++ app/Listeners/SendVideoCreatedNotification.php
@@ -4,10 +4,9 @@ namespace App\Listeners;
 
 use App\Events\VideoCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 
-class SendVideoCreatedNotification
+class SendVideoCreatedNotification implements ShouldQueue
 {
     /**
      * Handle the event.
