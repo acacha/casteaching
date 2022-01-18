dec743e056a0a46943772b0eb571008a134870eb
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-01-18 12:49:07 +0100

    Codi temporal del vídeo 130

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import VideosList from "./components/VideosList";
 import Alpine from 'alpinejs';
-import casteaching from 'casteaching'
+import casteaching from '@acacha/casteaching'
 import Vue from 'vue'
 import VideoForm from "./components/VideoForm";
 import Status from "./components/Status";
