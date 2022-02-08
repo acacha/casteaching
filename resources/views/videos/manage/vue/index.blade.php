fd4e5fa4eecc28da57dc8152f17308ecafe6efc2
Sergi Tur <sergiturbadenas@gmail.com>
2022-02-08 15:33:47 +0100

    Corregit petit error feia no anes el status a Vue Videos

--- resources/views/videos/manage/vue/index.blade.php
+++ resources/views/videos/manage/vue/index.blade.php
@@ -9,9 +9,9 @@
     <div class="flex flex-col mt-10">
 
         <div class="mx-auto sm:px-6 lg:px-8 w-full max-w-7xl">
-            <status></status>
-
             <div id="app">
+                <status></status>
+
                 @can('videos_manage_create')
                     <video-form></video-form>
                 @endcan
