fbe52f1bf586ffc3bd5df9b93a0241ba4e21236b
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-02-10 12:47:30 +0100

    Codi final vídeo 136

--- resources/views/series/manage/edit.blade.php
+++ resources/views/series/manage/edit.blade.php
@@ -11,62 +11,41 @@
         <div class="mx-auto sm:px-6 lg:px-8 w-full max-w-7xl">
 
             @can('series_manage_create')
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mb-1 md:mb-2 lg:mb-4">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="md:grid md:grid-cols-3 md:gap-6 bg-white md:bg-transparent">
-                            <div class="md:col-span-1">
-                                <div class="px-4 py-4 sm:px-6 md:px-4">
-                                    <h3 class="text-lg font-medium leading-6 text-gray-900">Vídeos</h3>
-                                    <p class="mt-1 text-sm text-gray-600">
-                                        Informació bàsica del vídeo
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="md:mt-0 md:col-span-2">
-                                <form data-qa="form_serie_edit" action="#" method="POST" >
-                                    @csrf
-                                    @method('PUT')
-                                    <div class="shadow sm:rounded-md sm:overflow-hidden md:bg-white">
-                                        <div class="px-4 py-5 space-y-6 sm:p-6">
 
-                                            <div>
-                                                <label for="title" class="block text-sm font-medium text-gray-700">
-                                                    Title
-                                                </label>
-                                                <div class="mt-1">
-                                                    <input required type="text" id="title" name="title" rows="3" class="shadow-sm mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" value="{{ $serie->title }}" ></input>
-                                                </div>
-                                                <p class="mt-2 text-sm text-gray-500">
-                                                    Titol curt del vídeo
-                                                </p>
-                                            </div>
+                <x-jet-form-section>
+                    <x-slot name="title">
+                        {{ __('Series') }}
+                    </x-slot>
 
-                                            <div>
-                                                <label for="description" class="block text-sm font-medium text-gray-700">
-                                                    Description
-                                                </label>
-                                                <div class="mt-1">
-                                                    <textarea required id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Description">{{ $serie->description }}</textarea>
-                                                </div>
-                                                <p class="mt-2 text-sm text-gray-500">
-                                                    Breu descripció del vídeo
-                                                </p>
-                                            </div>
+                    <x-slot name="description">
+                        {{ __('Informació bàsica de la sèrie') }}
+                    </x-slot>
 
-                                        </div>
-                                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                Modificar
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                    <x-slot name="actions">
+                        <x-jet-button>
+                            {{ __('Modifica') }}
+                        </x-jet-button>
+                    </x-slot>
+
+                    <x-slot name="form">
+                        @csrf
+                        @method('PUT')
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="title" value="{{ __('Title') }}" />
+                            <x-jet-input id="title" name="title" type="text" class="mt-1 block w-full" autocomplete="name" required value="{{ $serie->title }}"/>
+                            <x-jet-input-error for="title" class="mt-2" />
                         </div>
-                    </div>
-                </div>
+
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="description" value="{{ __('Description') }}" />
+                            <textarea required id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Description">{{ $serie->description }}</textarea>
+                            <x-jet-input-error for="description" class="mt-2" />
+                        </div>
+                    </x-slot>
+                </x-jet-form-section>
 
                 <x-jet-section-border />
+
                 <x-jet-form-section submit="TODO">
                     <x-slot name="title">
                         {{ __('Imatge de la sèrie') }}
@@ -77,12 +56,19 @@
                     </x-slot>
 
                     <x-slot name="form">
-                        TODO FORM
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="image" value="{{ __('Image') }}" />
+                            <input type="file"
+                                   id="image" name="image"
+                                   accept="image/png, image/jpeg">
+                            <x-jet-input-error for="image" class="mt-2" />
+                        </div>
+
                     </x-slot>
 
                     <x-slot name="actions">
                         <x-jet-button>
-                            {{ __('Save') }}
+                            {{ __('Modifica') }}
                         </x-jet-button>
                     </x-slot>
                 </x-jet-form-section>
