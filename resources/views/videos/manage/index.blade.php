c8572633f7547d02a377127b6080f5392d4927b2
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-02-10 11:13:14 +0100

    Neteja de comentaris ja no calen

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -73,84 +73,8 @@
                                 </select>
                                 <x-jet-input-error for="user" class="mt-2" />
                             </div>
-
-
-
-
-                        {{--                            <div class="px-4 py-5 space-y-6 sm:p-6">--}}
-
-{{--                                <div>--}}
-{{--                                    <label for="title" class="block text-sm font-medium text-gray-700">--}}
-{{--                                        Title--}}
-{{--                                    </label>--}}
-{{--                                    <div class="mt-1">--}}
-{{--                                        <input required type="text" id="title" name="title" rows="3" class="shadow-sm mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" placeholder="Titol del vídeo"></input>--}}
-{{--                                    </div>--}}
-{{--                                    <p class="mt-2 text-sm text-gray-500">--}}
-{{--                                        Titol curt del vídeo--}}
-{{--                                    </p>--}}
-{{--                                </div>--}}
-
-{{--                                <div>--}}
-{{--                                    <label for="description" class="block text-sm font-medium text-gray-700">--}}
-{{--                                        Description--}}
-{{--                                    </label>--}}
-{{--                                    <div class="mt-1">--}}
-{{--                                        <textarea required id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Description"></textarea>--}}
-{{--                                    </div>--}}
-{{--                                    <p class="mt-2 text-sm text-gray-500">--}}
-{{--                                        Breu descripció del vídeo--}}
-{{--                                    </p>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="grid grid-cols-3 gap-6">--}}
-{{--                                    <div class="col-span-3">--}}
-{{--                                        <label for="url" class="block text-sm font-medium text-gray-700">--}}
-{{--                                            URL--}}
-{{--                                        </label>--}}
-{{--                                        <div class="mt-1 flex rounded-md shadow-sm">--}}
-{{--                                                      <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">--}}
-{{--                                                        http://--}}
-{{--                                                      </span>--}}
-{{--                                            <input required type="url" name="url" id="url" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block  rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="youtube.com/">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-
-{{--                                <div class="grid grid-cols-3 gap-6">--}}
-{{--                                    <div class="col-span-3">--}}
-{{--                                        <label for="serie" class="block text-sm font-medium text-gray-700">--}}
-{{--                                            Serie--}}
-{{--                                        </label>--}}
-{{--                                        <select id="serie" name="serie_id" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">--}}
-{{--                                            <option value="">-- Escolliu una serie --</option>--}}
-{{--                                            @foreach (App\Models\Serie::all() as $serie)--}}
-{{--                                                <option value="{{ $serie->id }}"> {{ $serie->title }}</option>--}}
-{{--                                            @endforeach--}}
-{{--                                        </select>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="grid grid-cols-3 gap-6">--}}
-{{--                                    <div class="col-span-3">--}}
-{{--                                        <label for="user" class="block text-sm font-medium text-gray-700">--}}
-{{--                                            User--}}
-{{--                                        </label>--}}
-{{--                                        <select id="user" name="user_id" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">--}}
-{{--                                            <option value="">-- Escolliu un usuari --</option>--}}
-{{--                                            @foreach (App\Models\User::all() as $user)--}}
-{{--                                                <option value="{{ $user->id }}"> {{ $user->name }}</option>--}}
-{{--                                            @endforeach--}}
-{{--                                        </select>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                            </div>--}}
                     </x-slot>
                 </x-jet-form-section>
-
-
             @endcan
 
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mt-4">
