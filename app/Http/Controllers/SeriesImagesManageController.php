5c1dd2da1079183fc7c009c8e4cfc82f6ef7c084
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-02-11 12:10:13 +0100

    Validacions imatges. Final vídeo 138

--- app/Http/Controllers/SeriesImagesManageController.php
+++ app/Http/Controllers/SeriesImagesManageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Tests\Feature\Series\SeriesImagesManageControllerTest;
 
 class SeriesImagesManageController extends Controller
@@ -15,6 +16,10 @@ class SeriesImagesManageController extends Controller
 
     public function update(Request $request)
     {
+        $request->validate([
+//            'image' => ['image',Rule::dimensions()->minHeight(400)->ratio(1.777777778)],
+            'image' => ['image','dimensions:min_height=400,ratio=16/9'],
+        ]);
         $serie = Serie::findOrFail($request->id);
         $serie->image = $request->file('image')->store('series','public');
         $serie->save();
--- tests/Feature/Series/SeriesImagesManageControllerTest.php
+++ tests/Feature/Series/SeriesImagesManageControllerTest.php
@@ -32,17 +32,96 @@ class SeriesImagesManageControllerTest extends TestCase
 
         // URI ENDPOINT -> API -> FUNCTION
         $response = $this->put('/manage/series/' . $serie->id . '/image/',[
-            'image' => $file = UploadedFile::fake()->image('serie.jpg'),
+            'image' => $file = UploadedFile::fake()->image('serie.jpg',960,540),
         ]);
 
         $response->assertRedirect();
 
-        Storage::disk('public')->assertExists('/series/'. $file->hashName());
-
         $response->assertSessionHas('status', __('Successfully updated'));
 
-        $this->assertEquals($serie->refresh()->image,'series/'.$file->hashName());
-        $this->assertNotNull($serie->refresh()->image);
+        Storage::disk('public')->assertExists('/series/'. $file->hashName());
 
+        $this->assertEquals($serie->refresh()->image,'series/'.$file->hashName());
+        $this->assertNotNull($serie->image);
+        $this->assertFileEquals($file->getPathname(),Storage::disk('public')->path($serie->image));
+
+    }
+
+    /** @test */
+    public function series_image_have_to_be_an_image()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'TDD 101',
+            'description' => 'Aprèn tot sobre TDD',
+            'image' => 'series/anterior.png',
+            'teacher_name' => 'Sergi Tur'
+        ]);
+
+        Storage::fake('public');
+
+        // URI ENDPOINT -> API -> FUNCTION
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = UploadedFile::fake()->create('serie.pdf',0,'application/pdf'),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image');
+
+        $this->assertEquals('series/anterior.png',$serie->refresh()->image);
+
+
+    }
+
+    /** @test */
+    function series_image_must_be_at_least_400px_height()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'TDD 101',
+            'description' => 'Aprèn tot sobre TDD',
+            'image' => 'series/anterior.png',
+            'teacher_name' => 'Sergi Tur'
+        ]);
+
+        Storage::fake('public');
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = $file = UploadedFile::fake()->image('serie.jpg', 200, 399),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image');
+
+        $this->assertEquals('series/anterior.png',$serie->refresh()->image);
+    }
+
+    /** @test */
+    function series_image_must_be_aspect_ratio_16_9()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'TDD 101',
+            'description' => 'Aprèn tot sobre TDD',
+            'image' => 'series/anterior.png',
+            'teacher_name' => 'Sergi Tur'
+        ]);
+
+        Storage::fake('public');
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = $file = UploadedFile::fake()->image('serie.jpg', 6000, 400),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image',function($error){
+            dd($error);
+        });
+
+        $this->assertEquals('series/anterior.png',$serie->refresh()->image);
     }
 }
