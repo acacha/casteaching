e0c7dcaa24eb1e5efb802b1ffb2b13efa1367dc2
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-02-15 10:06:18 +0100

    Refactorització amb null coalescence operator

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -51,7 +51,7 @@ class Serie extends Model
     protected function imageUrl(): Attribute
     {
         return new Attribute(
-            get: fn ($value) => is_null($this->image) ? 'series/placeholder.png' : $this->image ,
+            get: fn ($value) => $this->image ?? 'series/placeholder.png',
         );
     }
 }
