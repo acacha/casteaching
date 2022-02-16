7e003b4dcd5695a702ba1e7535b16c290b818147
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-02-16 18:56:06 +0100

    Final vídeo 143. Un munt de detalls de UI com truncates de text massa llargs, textos alternatius, etc

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -50,7 +50,8 @@
                 </div>
 
                 <div class="flex-shrink-0 flex items-center px-4">
-                    TODO SERIE IMAGE AND SERIE TITLE
+                    <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0 mr-2" src="/storage/{{ $video->serie?->image_url }}" alt="">
+                    <span class="w-80 truncate">{{ $video->serie?->title }}</span>
                 </div>
                 <div class="mt-5 flex-1 h-0 overflow-y-auto">
                     <nav class="px-2">
@@ -58,7 +59,7 @@
                             @foreach ($videos_series as $sVideo)
                                 @if ($sVideo->is($video))
                                          <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:text-gray-900 hover:bg-gray-50" -->
-                                        <a href="/videos/{{ $sVideo->id }}" class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md" aria-current="page">
+                                        <a href="/videos/{{ $sVideo->id }}" class="w-80 bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md" aria-current="page">
                                             <!--
                                               Heroicon name: outline/home
 
@@ -67,15 +68,15 @@
                                             <svg class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                             </svg>
-                                            {{ $sVideo->title }}
+                                            <span class="w-64 truncate" title="{{ $sVideo->title }}">{{ $sVideo->title }}</span>
                                         </a>
                                     @else
-                                        <a href="/videos/{{ $sVideo->id }}" class="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md">
+                                        <a href="/videos/{{ $sVideo->id }}" class="w-80 text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md">
                                             <!-- Heroicon name: outline/view-list -->
                                             <svg class="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
                                             </svg>
-                                            {{ $sVideo->title }}
+                                            <span class="w-64 truncate" title="{{ $sVideo->title }}">{{ $sVideo->title }}</span>
                                         </a>
                                 @endif
                             @endforeach
@@ -91,7 +92,8 @@
         <!-- Static sidebar for desktop -->
         <div class="hidden lg:flex lg:flex-col lg:w-96 lg:fixed lg:top-[64px] lg:bottom-0 lg:border-r lg:border-gray-200 lg:pt-5 lg:pb-4 lg:bg-gray-100">
             <div class="flex items-center flex-shrink-0 px-6">
-                IMAGE SERIE | SERIE TITLE TODO
+                <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0 mr-2" src="/storage/{{ $video->serie?->image_url }}" alt="">
+                <span class="w-80 truncate" title="{{ $video->serie?->title }}">{{ $video->serie?->title }}</span>
             </div>
             <!-- Sidebar component, swap this element with another sidebar if you like -->
             <div class="mt-6 h-0 flex-1 flex flex-col">
@@ -101,9 +103,10 @@
                         <button type="button" class="group w-full bg-gray-100 rounded-md px-3.5 py-2 text-sm text-left font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500" id="options-menu-button" aria-expanded="false" aria-haspopup="true">
                             <span class="flex w-full justify-between items-center">
                               <span class="flex min-w-0 items-center justify-between space-x-3">
-                                <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" alt="">
+                                <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="{{ $video->serie?->teacher_photo_url}}" alt="{{ $video->serie?->teacher_name }}">
+
                                 <span class="flex-1 flex flex-col min-w-0">
-                                  <span class="text-gray-900 text-sm font-medium truncate">Teacher Name</span>
+                                  <span class="text-gray-900 text-sm font-medium truncate">{{ $video->serie?->teacher_name }}</span>
                                   <span class="text-gray-500 text-sm truncate">@jessyschwarz</span>
                                 </span>
                               </span>
@@ -130,7 +133,7 @@
                         @foreach ($videos_series as $sVideo)
                             @if ($sVideo->is($video))
                                 <!-- Current: "bg-gray-200 text-gray-900", Default: "text-gray-700 hover:text-gray-900 hover:bg-gray-50" -->
-                                <a href="#" class="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md" aria-current="page">
+                                <a href="#" class="w-80 bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md" aria-current="page">
                                     <!--
                                       Heroicon name: outline/home
 
@@ -139,14 +142,14 @@
                                     <svg class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                     </svg>
-                                    {{ $sVideo->title }}
+                                    <span class="w-80 truncate" title="{{ $sVideo->title }}">{{ $sVideo->title }}</span>
                                 </a>
                             @else
-                                    <a href="/videos/{{ $sVideo->id }}" class="text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                                    <a href="/videos/{{ $sVideo->id }}" class="w-80 text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
                                         <svg class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                         </svg>
-                                        {{ $sVideo->title }}
+                                        <span class="w-80 truncate" title="{{ $sVideo->title }}">{{ $sVideo->title }}</span>
                                     </a>
                             @endif
                         @endforeach
@@ -174,7 +177,8 @@
                                 <label for="search-field" class="sr-only">Search</label>
                                 <div class="relative w-full text-gray-400 focus-within:text-gray-600">
                                     <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                                      <-- Videos |  IMAGE SERIE | TODO NOM SERIE HERE
+                                      <span class="hidden md:inline md:mr-2"><--</span> <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0 mr-2" src="/storage/{{ $video->serie?->image_url }}" alt="">
+                                      <span class="w-48 truncate" title="{{ $video->serie?->title }}">{{ $video->serie?->title }}</span>
                                     </div>
                                 </div>
                             </div>
@@ -184,9 +188,8 @@
                             <div class="ml-3 relative">
                                 <div>
                                     <button type="button" class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                        <span class="sr-only">Open user menu</span>
-                                        <span class="mr-3">TODO teacher name</span>
-                                        <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                        <span class="mr-3 hidden md:inline">{{ $video->serie?->teacher_name }}</span>
+                                        <img class="h-8 w-8 rounded-full" src="{{ $video->serie?->teacher_photo_url }}" alt="">
                                     </button>
                                 </div>
 
--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Videos;
 
+use App\Models\Serie;
 use App\Models\User;
 use App\Models\Video;
 use Carbon\Carbon;
@@ -16,6 +17,43 @@ use Tests\TestCase;
 class VideoTest extends TestCase
 {
     use RefreshDatabase, CanLogin;        // ESTAT PRECONEGUT -> ZERO STATE
+
+    /**
+     * @test
+     */
+    public function users_can_view_video_series_navigation()
+    {
+        $serie = Serie::create([
+            'title' => 'Introducció a Ubuntu',
+            'description' => 'Bla bla bla',
+            'teacher_name' => 'Sergi Tur Badenas',
+            'teacher_photo_url' => $gravatar = 'https://www.gravatar.com/avatar/' . md5('sergiturbadenas@gmail.com')
+        ]);
+
+        $video = Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'serie_id' => $serie->id
+        ]);
+
+        $response = $this->get('/videos/' . $video->id); // SLUGS -> SEO -> TODO
+
+        $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+        $response->assertSee('Here description');
+        $response->assertSee('13 de desembre de 2020');
+        $response->assertSee('https://youtu.be/w8j07_DBl_I');
+
+        // NO ES MOSTRA LA NAVEGACIÓ DE SERIES
+        $response->assertSee('<div id="layout_series_navigation"',false);
+        $response->assertSee($serie->title);
+        $response->assertSee($serie->teacher_name);
+        $response->assertSee($gravatar);
+    }
 
     /**
      * @test
