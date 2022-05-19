2640aaaeae2631ab9ad4b7cfc275f323cbcfb718
Sergi Tur <sergiturbadenas@gmail.com>
2022-05-19 19:45:57 +0200

    Els || fan comparació lògica i tornen un boole+a i no l'string per defecte. Cambiat per ??. Gràcies a @palanka777 per veure-ho

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,7 +94,7 @@ class User extends Authenticatable
         $user = User::where('github_id', $githubUser->id)->first();
 
         if ($user) {
-            $user->name = $githubUser->name || 'Github User';
+            $user->name = $githubUser->name ?? 'Github User';
             $user->github_token = $githubUser->token;
             $user->github_refresh_token = $githubUser->refreshToken;
             $user->github_nickname = $githubUser->nickname;
@@ -112,7 +112,7 @@ class User extends Authenticatable
                 $user->save();
             } else {
                 $user = User::create([
-                    'name' => $githubUser->name || 'Github User',
+                    'name' => $githubUser->name ?? 'Github User',
                     'email' => $githubUser->email,
                     'password' => Hash::make(Str::random()),
                     'github_id' => $githubUser->id,
